cb7dc01ab5dda7692fac67f8c44aec7f22dd0318
Donovan <donovan.chong@digio.com.au>
2022-04-29 11:03:41 +1000

    Changed tables and secrets

--- devstack/localstack/start.sh
+++ devstack/localstack/start.sh
@@ -5,7 +5,7 @@ export AWS_SECRET_ACCESS_KEY=foobar
 
 aws --endpoint-url=http://localhost:4566 dynamodb create-table \
   --region=ap-southeast-2 \
-  --table-name dev-Persons \
+  --table-name Account \
   --attribute-definitions \
       AttributeName=ID,AttributeType=S \
   --key-schema \
@@ -13,43 +13,33 @@ aws --endpoint-url=http://localhost:4566 dynamodb create-table \
   --provisioned-throughput \
       ReadCapacityUnits=10,WriteCapacityUnits=5
 
-aws --endpoint-url=http://localhost:4566 dynamodb update-table \
-  --region=ap-southeast-2 \
-  --table-name dev-Persons \
-  --attribute-definitions AttributeName=HashedEmail,AttributeType=S \
-  --global-secondary-index-updates \
- "[{\"Create\":{\"IndexName\": \"HashedEmailIndex\",\"KeySchema\":[{\"AttributeName\":\"HashedEmail\",\"KeyType\":\"HASH\"}], \
- \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5},
- \"Projection\":{\"ProjectionType\":\"INCLUDE\", \"NonKeyAttributes\": [\"ID\"]}}}]" \
+#aws --endpoint-url=http://localhost:4566 dynamodb update-table \
+#  --region=ap-southeast-2 \
+#  --table-name Account \
+#  --attribute-definitions AttributeName=HashedEmail,AttributeType=S \
+#  --global-secondary-index-updates \
+# "[{\"Create\":{\"IndexName\": \"HashedEmailIndex\",\"KeySchema\":[{\"AttributeName\":\"HashedEmail\",\"KeyType\":\"HASH\"}], \
+# \"ProvisionedThroughput\": {\"ReadCapacityUnits\": 10, \"WriteCapacityUnits\": 5},
+# \"Projection\":{\"ProjectionType\":\"INCLUDE\", \"NonKeyAttributes\": [\"ID\"]}}}]" \
 
 aws --endpoint-url=http://localhost:4566 dynamodb create-table \
   --region=ap-southeast-2 \
-  --table-name dev-TeamMember \
+  --table-name Purchases \
   --attribute-definitions \
       AttributeName=ID,AttributeType=S \
-      AttributeName=Division,AttributeType=S \
   --key-schema \
       AttributeName=ID,KeyType=HASH \
-      AttributeName=Division,KeyType=RANGE \
   --provisioned-throughput \
       ReadCapacityUnits=10,WriteCapacityUnits=5 \
   --stream-specification \
       StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES
 
-aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name /dev/adv-account/auth0 --secret-string \
+aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name client_secret_1 --secret-string \
   --region=ap-southeast-2 \
-  "{\"domain\":\"auth.dev.onepass.com.au\", \
-    \"clientId\":\"foobar\", \
+  "{\"clientId\":\"foobar\", \
     \"secret\":\"boofar\"}"
 
-aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name /dev/adv-account/auth0-rwa --secret-string \
+aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name client_secret_2 --secret-string \
   --region=ap-southeast-2 \
-  "{\"domain\":\"auth.dev.onepass.com.au\", \
-    \"clientId\":\"foobar\", \
-    \"secret\":\"boofar\"}"
-
-aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name /dev/adv-schema-registry/auth/basic_auth_credentials --secret-string \
-  --region=ap-southeast-2 \
-  "{\"group\":\"readwrite\", \
-    \"password\":\"schema\", \
-    \"username\":\"schema\"}"
+  "{\"clientId\":\"foobar2\", \
+    \"secret\":\"boofar2\"}"
