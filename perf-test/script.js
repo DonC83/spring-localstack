9751ed19c8f94549e34e8096e7a11f825a94f242
Donovan <donovan.chong@digio.com.au>
2022-05-02 15:34:52 +1000

    Updated docker compose command in runner

---
+++ perf-test/script.js
@@ -0,0 +1,37 @@
+import http from 'k6/http'
+import { fail, check, sleep } from 'k6';
+
+const baseUrl = "http://localhost:8080";
+
+export const options = {
+  vus: 10,
+  duration: '30s'
+}
+
+export function setup() {
+  let started = false;
+  while (!started) {
+    let resp = http.get(`${baseUrl}/health`);
+    started = JSON.parse(resp.body) != null;
+    sleep(10);
+  }
+
+  let healthStatus = "";
+  while (healthStatus !== "UP") {
+    let resp = http.get(`${baseUrl}/health`);
+    healthStatus = JSON.parse(resp.body).status;
+    sleep(5);
+  }
+  console.log("Set up complete");
+}
+
+export default function() {
+
+  const resp = http.get(`${baseUrl}/secret/client_secret_1`)
+
+  if (!check(resp, {
+    'get secret status is 200': (r) => r.status === 200
+  })) {
+    fail(`Failed to get secret: http status = ${resp.status}`);
+  }
+}
