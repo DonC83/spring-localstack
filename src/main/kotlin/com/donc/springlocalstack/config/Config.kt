b8fd87d90c7047e73b4ae9cab1419f1ed39a576c
Donovan <donovan.chong@digio.com.au>
2022-05-02 21:51:08 +1000

    Added config to test secret retrieval on application starting

--- src/main/kotlin/com/donc/springlocalstack/config/Config.kt
+++ src/main/kotlin/com/donc/springlocalstack/config/Config.kt
@@ -1,31 +1,29 @@
 package com.donc.springlocalstack.config
 
+import com.donc.springlocalstack.model.Secret
+import com.donc.springlocalstack.repository.SecretService
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider
-import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient
-import software.amazon.awssdk.regions.Region
-import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient
-import java.net.URI
 
 @Configuration
 class Config(
-    @Value("\${aws.region}") private val region: String,
-    @Value("\${aws.endpoint}") private val endpoint: String,
-    private val credentialsProvider: AwsCredentialsProvider
+    secretService: SecretService,
+    @Value("\${client.secret.name}") private val clientSecretName: String
 ) {
 
-    @Bean
-    fun dynamoDbEnhancedAsyncClient(): DynamoDbEnhancedAsyncClient {
-        val client = DynamoDbAsyncClient.builder()
-            .region(Region.of(region))
-            .endpointOverride(URI.create(endpoint))
-            .credentialsProvider(credentialsProvider)
-            .build()
+    private val secret = secretService
+        .getSecretValue(clientSecretName, Secret::class.java).secret
+        .let { secret ->
+            if (secret.equals("funnyman")) {
+                "alternative"
+            } else {
+                secret
+            }
+        }
 
-        return DynamoDbEnhancedAsyncClient.builder()
-            .dynamoDbClient(client)
-            .build()
+    @Bean
+    fun getSecret(): String {
+        return secret
     }
 }
