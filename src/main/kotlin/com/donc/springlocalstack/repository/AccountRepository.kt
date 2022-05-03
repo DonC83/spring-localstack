df7b140abab25d123c05b243a82d025efc07bbd8
Donovan <donovan.chong@digio.com.au>
2022-05-03 16:12:51 +1000

    Added test on config loading;

--- src/main/kotlin/com/donc/springlocalstack/repository/AccountRepository.kt
+++ src/main/kotlin/com/donc/springlocalstack/repository/AccountRepository.kt
@@ -1,18 +1,37 @@
 package com.donc.springlocalstack.repository
 
 import com.donc.springlocalstack.model.Account
+import com.donc.springlocalstack.model.Secret
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Repository
 import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedAsyncClient
 import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 
 @Repository
 class AccountRepository(
-    client: DynamoDbEnhancedAsyncClient
+    client: DynamoDbEnhancedAsyncClient,
+    secretService: SecretService,
+    @Value("\${client.secret.name}") private val clientSecretName: String
 ) {
+
+
+    private val secret = {
+        secretService
+            .getSecretValue(clientSecretName, Secret::class.java).secret
+            .let { secret ->
+                println("$$$$ $secretService")
+                if (secret == "funnyman") {
+                    "alternative"
+                } else {
+                    secret
+                }
+            }
+    }
 
     private val table = client.table("Account", TableSchema.fromBean(Account::class.java))
 
     fun save(account: Account) {
+        println(secret)
         table.putItem(account)
     }
 }
